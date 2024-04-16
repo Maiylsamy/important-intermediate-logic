0c71def1f32b13e0de5e39e531d8588b5e5290d5
Maiylsamy D <139238726+Maiylsamy@users.noreply.github.com>
2024-04-16 08:19:12 +0530

    Update abundant number.py

--- abundant number.py
+++ abundant number.py
@@ -1,3 +1,7 @@
+# The Factors for the number 12 are, 1, 2, 3, 4 and 6. We don't want to include the number itself.
+# Now the sum of the factors except the number itself is :1 + 2 + 3 + 4 + 6 = 16
+# as the number 16>12 , the number itself.
+# It's an abundant number.
 n = int(input('enter a no:'))
 l1 = 0
 for i in range(1,n):
