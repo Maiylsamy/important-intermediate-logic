0ec2333e839473b49296a27edc7b7c09d1e65558
Maiylsamy D <139238726+Maiylsamy@users.noreply.github.com>
2024-04-16 08:21:07 +0530

    Update amstrong in range.py

--- amstrong in range.py
+++ amstrong in range.py
@@ -1,3 +1,6 @@
+# Number = 5
+# Square of number = 25
+# as the square of the number ends with the number itself, It's an Automorphic number.
 i = int(input('enter a no'))
 j =int( input('enter a no'))
 for n in range(i,j):
