84b4bc6e5892c106f992d448e8a0e5beac7da10f
Maiylsamy D <139238726+Maiylsamy@users.noreply.github.com>
2024-04-16 08:24:31 +0530

    Update automorphic number.py

--- automorphic number.py
+++ automorphic number.py
@@ -1,3 +1,6 @@
+# Number = 5
+# Square of number = 25
+# as the square of the number ends with the number itself, It's an Automorphic number.
 n = int(input('enter a no:'))
 square = pow(n,2)
 mod = pow(10,len(str(n)))
